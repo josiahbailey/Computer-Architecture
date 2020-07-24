4899a2ebc14683ff84985ce9d3ccdbe5cd0b71e3
Josiah <reikiryo@yahoo.com>
2020-07-24 11:28:53 -0400

    Day 4 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,7 +1,5 @@
 """CPU functionality."""
 
-import sys
-
 
 class CPU:
     """Main CPU class."""
@@ -11,6 +9,7 @@ class CPU:
         self.memory = [0] * 256
         self.reg = [0] * 8
         self.stack = -1
+        self.address = 0
         self.running = False
 
     def ram_read(self, address):
@@ -29,16 +28,15 @@ class CPU:
                 line = line.split("#")
                 try:
                     v = int(line[0], 2)
-                    # print(v)
                     program.append(v)
                 except ValueError:
                     continue
 
-                address = 0
-
                 for instruction in program:
-                    self.ram_write(instruction, address)
-                    address += 1
+                    self.ram_write(instruction, self.address)
+                    self.address += 1
+
+                self.address = 0
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -52,88 +50,67 @@ class CPU:
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
-
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            # self.fl,
-            # self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
-
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
-        print()
-
     def run(self):
         """Run the CPU."""
         self.running = True
 
-        def HLT(z, x):
+        def HLT(x, y):
             self.running = False
 
         def LDI(register, value):
             self.reg[register] = value
 
-        def PRN(register, z):
+        def PRN(register, y):
             print(self.reg[register])
 
-        def MLT(op, reg_1, reg_2):
-            self.alu(op, reg_1, reg_2)
+        def MLT(reg_1, reg_2):
+            self.alu("MLT", reg_1, reg_2)
 
-        def PSH(register, z):
+        def ADD(reg_1, reg_2):
+            self.alu("ADD", reg_1, reg_2)
+
+        def PSH(register, y):
             self.memory.append(self.reg[register])
             self.stack -= 1
             del self.memory[self.stack]
 
-        def POP(register, z):
+        def POP(register, y):
             LDI(register, self.memory.pop())
             self.stack += 1
             self.memory.insert(self.stack, 0)
 
+        def CAL(register, y):
+            LDI(7, self.address)
+            PSH(7, y)
+            self.address = self.reg[register]
+
+        def RET(x, y):
+            POP(7, y)
+            self.address = self.reg[7]
+
         ops = {
-            1: ["HLT", 0, None],
-            130: ["LDI", 2, None],
-            71: ["PRN", 1, None],
-            162: ["MLT", 2, "ALU"],
-            69: ["PSH", 1, "STK"],
-            70: ["POP", 1, "STK"]
+            1: [HLT, 0],
+            17: [RET, 1],
+            69: [PSH, 1],
+            70: [POP, 1],
+            71: [PRN, 1],
+            80: [CAL, -1],
+            130: [LDI, 2],
+            160: [ADD, 2],
+            162: [MLT, 2]
         }
-
-        ops_func = {
-            "HLT": HLT,
-            "LDI": LDI,
-            "PRN": PRN,
-            "MLT": MLT,
-            "PSH": PSH,
-            "POP": POP
-        }
-
-        address = 0
 
         while self.running:
-            memory_value1 = self.ram_read(address)
-            memory_value2 = self.ram_read(address + 1)
-            memory_value3 = self.ram_read(address + 2)
+            memory_value1 = self.ram_read(self.address)
+            memory_value2 = self.ram_read(self.address + 1)
+            memory_value3 = self.ram_read(self.address + 2)
             operation = ops[memory_value1]
-            operation_func = ops_func[operation[0]]
 
-            if operation[2] == "ALU":
-                operation_func(operation[0], memory_value2, memory_value3)
-            else:
-                operation_func(memory_value2, memory_value3)
+            operation[0](memory_value2, memory_value3)
 
-            address += operation[1]
-            address += 1
+            self.address += operation[1] + 1
 
 
 cpu = CPU()
-cpu.load('stack.ls8')
+cpu.load('call.ls8')
 cpu.run()
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 
 """Main."""
-
-import sys
 from cpu import *
 
 cpu = CPU()
-
-cpu.load('print8.ls8')
+cpu.load('call.ls8')
 cpu.run()
